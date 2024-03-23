27ece4bbd223ff33f486135d9fc88607b67d2212
Bryan Quille <bryanquille@outlook.com>
2024-03-23 17:05:26 -0500

    Fixing scrolling in mobile menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Menu Mobile 
-
+const body = document.querySelector("body");
 const navbar = document.querySelector("#navbar");
 const openMenu = document.querySelector("#open-menu");
 const closeMenu = document.querySelector("#close-menu");
@@ -7,15 +7,18 @@ const navItems = document.querySelectorAll(".navitem");
 
 openMenu.addEventListener("click", () => {
     navbar.classList.add("visible");
+    body.setAttribute("style", "overflow: hidden;");
 });
 
 closeMenu.addEventListener("click", () => {
     navbar.classList.remove("visible");
+    body.removeAttribute("style");
 });
 
 const navItemsArray = Array.from(navItems);
 navItemsArray.forEach((item) => {
     item.addEventListener("click", () => {
         navbar.classList.remove("visible");
+        body.removeAttribute("style");
     });
 });
